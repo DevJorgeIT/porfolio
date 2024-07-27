398be34e89c8c1e1f19a20e8bf194598f27a23f4
Jorge Herraiz <144723068+jherraizsoler@users.noreply.github.com>
2024-07-27 16:34:13 +0200

    cambios

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
+/** Ventana emergente 1*/ 
 const open1 = document.getElementById('open1');
-const open2 = document.getElementById('open2');
 const modal_container1 = document.getElementById('modal_container1');
-const modal_container2 = document.getElementById('modal_container2');
 const close1 = document.getElementById('close1');
-const close2 = document.getElementById('close2');
 
 open1.addEventListener('click',() => {
     modal_container1.classList.add('show');
-});
-
-open2.addEventListener('click',() => {
-    modal_container2.classList.add('show');
+    
 });
 
 close1.addEventListener('click', () => {
     modal_container1.classList.remove('show');
 });
 
+/** Ventana emergente 2*/ 
+const open2 = document.getElementById('open2');
+const modal_container2 = document.getElementById('modal_container2');
+const close2 = document.getElementById('close2');
+
+
+open2.addEventListener('click',() => {
+    modal_container2.classList.add('show');
+});
+
 close2.addEventListener('click', () => {
     modal_container2.classList.remove('show');
 });
+
